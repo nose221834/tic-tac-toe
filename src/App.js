798ff7fc8deb42b1c27e31f303db4775ae2efa28
nose221834 <takumi.ichinose@fuller.co.jp>
2023-10-11 17:31:11 +0900

    to be contined

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function Board({xIsNext, squares, onPlay }) {
 
 export default function Game() {
   const [xIsNext,setXIsNext] = useState(true);
-  const [history, setHistory] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
 
@@ -68,20 +68,40 @@ export default function Game() {
     setHistory([...history,nextSquares]);
     setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove) {
+
+  }
+
+  const moves = history.map((squares,move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    }else {
+    description = 'Go to game start';
+    }
+    
+    return (
+      <li>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+
+
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
 }
-
-
-
 
 
 function calculateWinner(squares) {
